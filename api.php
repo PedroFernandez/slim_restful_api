446747f61a62617af6d1eeb6dc879e3769be2474
PedroFernandez <fdez.pedro@gmail.com>
2017-11-12 13:39:03 +0100

    add products to db

--- api.php
+++ api.php
@@ -18,4 +18,21 @@ $app->get('/products', function () use($app, $db) {
     echo json_encode($products);
 });
 
+$app->post('/products', function () use($db, $app) {
+   $query = "INSERT INTO products VALUES (null,"
+            ." '{$app->request->post("name")}',"
+            ." '{$app->request->post("description")}',"
+            ." '{$app->request->post("price")}'"
+            .")";
+    $insert = $db->query($query);
+
+    if ($insert) {
+        $result = ['result' => true, 'message' => 'Product has been added correctly'];
+    } else {
+        $result = ['result' => false, 'message' => 'Product has NOT been added correctly'];
+    }
+
+    echo json_encode($result);
+});
+
 $app->run();
